a6102b483b2acae4c4e295e94d4219056d8c37c3
Mohamed Ashraf <botoashraf123@gmail.com>
2022-04-16 04:34:32 +0200

    edit src paths

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
+
+	//Loading page animation
+	$('.preloader').fadeOut(2000, function () {
+
+		$('body, html').css('overflow-y', 'visible');
+	});
+
+	//Show scroll btn
+	var secOff = $('#bonus, #about').offset().top;
+
+			$(window).on('scroll', function() {
+				if ($(this).scrollTop() >= secOff) {
+					$('.scroll-btn').removeClass('hide')
+				} else {
+					$('.scroll-btn').addClass('hide');
+				}
+			})
+	
+		// collapse the menu on clicking any link
+		$('.navbar-nav .nav-item .nav-link:not(".dropdown-toggle"), .navbar-nav .nav-item .dropdown-item').click(() => {
+			$('.navbar-collapse').removeClass('show');
+			$('.navbar-toggler').attr('aria-expanded', false).addClass('collapsed')
+		})
+
 	// Video Popup
 	$('.popup-youtube').magnificPopup({
 		disableOn: 700,
